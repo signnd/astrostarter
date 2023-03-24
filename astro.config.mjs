ca0c8e0c9133cd77171d1fd1c3d43b4d80c4f2be
signnd <nfffqdb@gmail.com>
2023-03-24 14:17:14 +0800

    unit 6 done

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    //site: 'https://signnd.github.io',
-    //base: '/astrostarter'
-});
+  integrations: [preact()]
+} //site: 'https://signnd.github.io',
+//base: '/astrostarter'
+);
